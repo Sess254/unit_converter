74c96c4fac09f2b48e169d4b3096d239f1dfc5cd
Sess254 <wamaritemoses@gmail.com>
2024-05-12 18:30:30 +0300

    minor changes to code, done now

--- index.js
+++ index.js
@@ -23,19 +23,17 @@ function convertLength (userInput) {
    if (userInput === "" || isNaN(Number(userInput))){
     alert(`please enter a number`);
    } else {
-    let metric = userInput;
-    let imperial = userInput;
     const conversionLengthFactor = 3.281;
     const conversionVolumeFactor = 0.264;
     const conversionMassFactor = 2.204;
 
-    let resultLength = `${metric} meters = ${(metric * conversionLengthFactor).toFixed(3)} feet | ${imperial} feet = ${(metric / conversionLengthFactor).toFixed(3)} meters`;
+    let resultLength = `${userInput} meters = ${(userInput * conversionLengthFactor).toFixed(3)} feet | ${userInput} feet = ${(userInput / conversionLengthFactor).toFixed(3)} meters`;
     lengthEl.textContent = resultLength;
 
-    let resultVolume = `${metric} liters = ${(metric * conversionVolumeFactor).toFixed(3)} gallons | ${imperial} gallons = ${(metric / conversionVolumeFactor).toFixed(3)} liters`;
+    let resultVolume = `${userInput} liters = ${(userInput * conversionVolumeFactor).toFixed(3)} gallons | ${userInput} gallons = ${(userInput / conversionVolumeFactor).toFixed(3)} liters`;
     volumeEl.textContent = resultVolume;
    
-    let resultMass = `${metric} kilograms = ${(metric * conversionMassFactor).toFixed(3)} pounds | ${imperial} pounds = ${(metric / conversionMassFactor).toFixed(3)} kilogramms`;
+    let resultMass = `${userInput} kilograms = ${(userInput * conversionMassFactor).toFixed(3)} pounds | ${userInput} pounds = ${(userInput / conversionMassFactor).toFixed(3)} kilos`;
     massEl.textContent = resultMass;
    }
 
