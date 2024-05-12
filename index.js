094d97353180b8397f263b11d12825bb43b1b178
Sess254 <wamaritemoses@gmail.com>
2024-05-12 18:04:28 +0300

    made sure input is always a number plus more styling

--- index.js
+++ index.js
@@ -20,20 +20,26 @@ convertEl.addEventListener("click", function () {
 function convertLength (userInput) {
    userInput = inputEl.value;
 
-   let metric = userInput;
-   let imperial = userInput;
-   const conversionLengthFactor = 3.281;
-   const conversionVolumeFactor = 0.264;
-   const conversionMassFactor = 2.204;
+   if (userInput === "" || isNaN(Number(userInput))){
+    alert(`please enter a number`);
+   } else {
+    let metric = userInput;
+    let imperial = userInput;
+    const conversionLengthFactor = 3.281;
+    const conversionVolumeFactor = 0.264;
+    const conversionMassFactor = 2.204;
 
-   let resultLength = `${metric} meters = ${(metric * conversionLengthFactor).toFixed(3)} feet | ${imperial} feet = ${(metric / conversionLengthFactor).toFixed(3)} meters`;
-   lengthEl.textContent = resultLength;
+    let resultLength = `${metric} meters = ${(metric * conversionLengthFactor).toFixed(3)} feet | ${imperial} feet = ${(metric / conversionLengthFactor).toFixed(3)} meters`;
+    lengthEl.textContent = resultLength;
 
-   let resultVolume = `${metric} liters = ${(metric * conversionVolumeFactor).toFixed(3)} gallons | ${imperial} gallons = ${(metric / conversionVolumeFactor).toFixed(3)} liters`;
-   volumeEl.textContent = resultVolume;
+    let resultVolume = `${metric} liters = ${(metric * conversionVolumeFactor).toFixed(3)} gallons | ${imperial} gallons = ${(metric / conversionVolumeFactor).toFixed(3)} liters`;
+    volumeEl.textContent = resultVolume;
    
-   let resultMass = `${metric} kilograms = ${(metric * conversionMassFactor).toFixed(3)} pounds | ${imperial} pounds = ${(metric / conversionMassFactor).toFixed(3)} kilogramms`;
-   massEl.textContent = resultMass;
+    let resultMass = `${metric} kilograms = ${(metric * conversionMassFactor).toFixed(3)} pounds | ${imperial} pounds = ${(metric / conversionMassFactor).toFixed(3)} kilogramms`;
+    massEl.textContent = resultMass;
+   }
+
+   
 }
 
 
